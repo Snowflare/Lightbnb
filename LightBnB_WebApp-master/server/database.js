4306acd568b089dec5e4a29af7ac11b0f6e350d9
Daniel Zhang <danielzhang95@hotmail.com>
2020-05-29 01:35:58 +0000

    LighthouseBnB Add Property completed

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -125,7 +125,7 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  FULL JOIN property_reviews ON properties.id = property_id
   `;
 
   // 3
@@ -145,17 +145,17 @@ const getAllProperties = function(options, limit = 10) {
   if (options.minimum_price_per_night) {
     queryParams.push(`${options.minimum_price_per_night}00`);
     if (queryParams.length === 1) {
-      queryString += `WHERE cost_per_night > $${queryParams.length} `;
+      queryString += `WHERE cost_per_night >= $${queryParams.length} `;
     } else {
-      queryString += `AND cost_per_night > $${queryParams.length} `;
+      queryString += `AND cost_per_night >= $${queryParams.length} `;
     }
   }
   if (options.maximum_price_per_night) {
     queryParams.push(`${options.maximum_price_per_night}00`);
     if (queryParams.length === 1) {
-      queryString += `cost_per_night < $${queryParams.length} `;
+      queryString += `cost_per_night <= $${queryParams.length} `;
     } else {
-      queryString += `AND cost_per_night < $${queryParams.length} `;
+      queryString += `AND cost_per_night <= $${queryParams.length} `;
     }
   }
   
@@ -167,7 +167,7 @@ const getAllProperties = function(options, limit = 10) {
   GROUP BY properties.id `;
   if (options.minimum_rating) {
     queryParams.push(`${options.minimum_rating}`);
-    queryString += `HAVING avg(property_reviews.rating) > $${queryParams.length} `;    
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;    
   }
 
   queryParams.push(limit);
@@ -192,10 +192,25 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  console.log(property);
+  
+  return pool.query(`
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, 
+    cost_per_night, street, city, province, post_code, country,
+    parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night,
+  property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms,
+  property.number_of_bedrooms])
+  .then(res => {if (res.rows) {
+    console.log(res.rows);
+    
+    return res.rows[0];
+  } else {
+    return null;
+  }
+});
 }
 exports.addProperty = addProperty;
 
